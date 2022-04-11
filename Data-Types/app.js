66b80ca4feadcd85f0f25ba1c7b25b7d98eaf3fa
Ali Qadimi <aliqadimi19988@gmail.com>
2022-04-11 16:21:55 +0430

    Data-Types

---
+++ Data-Types/app.js
@@ -0,0 +1,57 @@
+//data-types
+//string
+let Variable = "qadimi";
+//number
+let number = 12;
+
+//object
+
+let Person = {
+    "name": "ali",
+    "age": 22,
+    "job": "fullStackDeveloper"
+}
+
+//array
+
+let Students = [1, 2, 65, 78]
+    // console.log(typeof(Students));
+
+
+
+
+//function
+
+let func = function() {
+    console.log("is Working");
+}
+
+// func()
+
+
+//Boolean
+
+Variable = true;
+
+//Date 
+
+let date = new Date();
+
+//undefined
+
+let name;
+
+
+//null
+
+let names = null;
+
+
+console.log(names);
+console.log(typeof(names));
+
+//check is  array
+// console.log(Array.isArray(Students));
+// console.log(Person.job);
+//Or
+// console.log(Person['job']);
