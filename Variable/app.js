da18c72e55fccea4aca915841ed271186a22476e
Ali Qadimi <aliqadimi19988@gmail.com>
2022-04-11 14:53:10 +0430

    let

--- Variable/app.js
+++ Variable/app.js
@@ -8,3 +8,17 @@ console.log(course);
 console.log(firstName);
 console.log(age);
 console.log(price);
+
+// let & const    ES6
+let number = 24
+number = 25
+number = 21
+
+
+var name = 'mamareza';
+name = 'akbar'
+name = 'hamid'
+
+
+console.log(number)
+console.log(name)
