dd20b59ce4323ff0ea86da57f4ce43687418d024
Ali Qadimi <aliqadimi19988@gmail.com>
2022-04-11 16:50:09 +0430

    Operators

---
+++ Operators/app.js
@@ -0,0 +1,46 @@
+//Operators
+
+let x = 20;
+let y = 10;
+let z = '20';
+let m;
+
+//Arithmetic
+m = x + y; //30
+m = x - y; //10
+m = x * y; //200
+m = x / y; //2
+m = x % y; // 0
+
+
+//Assigment
+
+x += y // x = x +y ;
+x -= y
+x *= y
+x /= y
+
+
+//Comparison 
+x = 20;
+m = x == y;
+m = x == z; // 20 true
+m = x === z; // flase 
+m = x != y;
+m = x > y;
+m = x < y;
+
+// Logical
+m = (x > y && y < x);
+
+// ||
+// &&
+
+
+
+
+
+console.log(m)
+    // console.log(x)
+    // console.log(y)
+    // console.log(z)
