abaaf967505096d76448f87b0394bbe24045fee8
Ali Qadimi <aliqadimi19988@gmail.com>
2022-04-11 14:48:17 +0430

    var

---
+++ Variable/app.js
@@ -0,0 +1,10 @@
+// var
+var course = "javascript",
+    firstName = "ali",
+    age = 22,
+    price = 5 * 2;
+
+console.log(course);
+console.log(firstName);
+console.log(age);
+console.log(price);
